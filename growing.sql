2cbd52129d124c32f153c79957cc7c64eafb9a56
cloudsystems22 <cloudsystems22@gmail.com>
2020-05-11 17:05:58 -0300

    Alterações no script para o banco de dados

--- growing.sql
+++ growing.sql
@@ -19,17 +19,18 @@ USE `growingdb` ;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `growingdb`.`usuarios` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(150) NOT NULL,
-  `sobrenome` VARCHAR(150) NULL,
-  `username` VARCHAR(150) NOT NULL,
-  `password` VARCHAR(256) NOT NULL,
-  `apelido` VARCHAR(150) NULL,
-  `genero` VARCHAR(50) NULL,
-  `image` VARCHAR(150) NULL,
-  `rg` VARCHAR(14) NULL,
-  `cpf` VARCHAR(14) NULL,
+  `nome_completo` VARCHAR(200) NOT NULL,
+  `nome_social` VARCHAR(150) NULL,
+  `genero` CHAR(1) NOT NULL,
+  `email` VARCHAR(100) NOT NULL,
+  `senha` VARCHAR(256) NOT NULL,
+  `foto_perfil` VARCHAR(200) NULL,
+  `cpf` VARCHAR(14) NOT NULL,
   `datanasc` DATE NULL,
-  PRIMARY KEY (`id`))
+  `descricao_bio` VARCHAR(500) NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE,
+  UNIQUE INDEX `cpf_UNIQUE` (`cpf` ASC) VISIBLE)
 ENGINE = InnoDB;
 
 
@@ -59,13 +60,14 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `growingdb`.`enderecos` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `tipo_endereco` VARCHAR(50) NOT NULL,
-  `tipo` VARCHAR(50) NOT NULL,
+  `tipo_logradouro` VARCHAR(50) NOT NULL,
   `logradouro` VARCHAR(150) NOT NULL,
-  `numero` VARCHAR(10) NOT NULL,
-  `bairro` VARCHAR(150) NULL,
-  `cep` VARCHAR(15) NULL,
-  `municipio` VARCHAR(150) NULL,
-  `estado` VARCHAR(50) NULL,
+  `numero` INT NOT NULL,
+  `complemento` VARCHAR(45) NULL,
+  `bairro` VARCHAR(150) NOT NULL,
+  `cep` VARCHAR(9) NOT NULL,
+  `municipio` VARCHAR(150) NOT NULL,
+  `uf_estado` CHAR(2) NOT NULL,
   `usuarios_id` INT NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_endereco_usuarios1_idx` (`usuarios_id` ASC) VISIBLE,
@@ -78,16 +80,19 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `growingdb`.`tipo_usuario`
+-- Table `growingdb`.`dados_profissionais`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `growingdb`.`tipo_usuario` (
+CREATE TABLE IF NOT EXISTS `growingdb`.`dados_profissionais` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `tipo_usuario` VARCHAR(50) NOT NULL,
-  `status` INT NOT NULL,
+  `cnpj` VARCHAR(18) NOT NULL,
+  `razao_social` VARCHAR(250) NULL,
+  `nome_fantasia` VARCHAR(150) NULL,
+  `inscricao_estadual` VARCHAR(14) NOT NULL,
+  `inscricao_municipal` VARCHAR(14) NULL,
   `usuarios_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_tipo_usuario_usuarios1_idx` (`usuarios_id` ASC) VISIBLE,
-  CONSTRAINT `fk_tipo_usuario_usuarios1`
+  INDEX `fk_dados_profissionais_usuarios1_idx` (`usuarios_id` ASC) VISIBLE,
+  CONSTRAINT `fk_dados_profissionais_usuarios1`
     FOREIGN KEY (`usuarios_id`)
     REFERENCES `growingdb`.`usuarios` (`id`)
     ON DELETE NO ACTION
@@ -96,67 +101,12 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `growingdb`.`perfil`
+-- Table `growingdb`.`preferencias_gerais`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `growingdb`.`perfil` (
+CREATE TABLE IF NOT EXISTS `growingdb`.`preferencias_gerais` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `tipo_perfil` VARCHAR(50) NOT NULL,
-  `razaosocial` VARCHAR(250) NOT NULL,
-  `nome_fantasia` VARCHAR(150) NULL,
-  `cnpj_cpf` VARCHAR(14) NOT NULL,
-  `ie_rg` VARCHAR(14) NOT NULL,
-  `im` VARCHAR(14) NULL,
-  `descricao` VARCHAR(500) NULL,
-  `image` VARCHAR(150) NULL,
-  `tipo_usuario_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_perfil_tipo_usuario1_idx` (`tipo_usuario_id` ASC) VISIBLE,
-  CONSTRAINT `fk_perfil_tipo_usuario1`
-    FOREIGN KEY (`tipo_usuario_id`)
-    REFERENCES `growingdb`.`tipo_usuario` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `growingdb`.`categorias`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `growingdb`.`categorias` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `categorias` VARCHAR(45) NOT NULL,
+  `servico` VARCHAR(50) NULL,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `growingdb`.`preferencias`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `growingdb`.`preferencias` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `preferencia` VARCHAR(150) NULL,
-  `tipo_usuario_id` INT NOT NULL,
-  `categorias_id` INT NOT NULL,
-  `perfil_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_preferencias_tipo_usuario1_idx` (`tipo_usuario_id` ASC) VISIBLE,
-  INDEX `fk_preferencias_categorias1_idx` (`categorias_id` ASC) VISIBLE,
-  INDEX `fk_preferencias_perfil1_idx` (`perfil_id` ASC) VISIBLE,
-  CONSTRAINT `fk_preferencias_tipo_usuario1`
-    FOREIGN KEY (`tipo_usuario_id`)
-    REFERENCES `growingdb`.`tipo_usuario` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_preferencias_categorias1`
-    FOREIGN KEY (`categorias_id`)
-    REFERENCES `growingdb`.`categorias` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_preferencias_perfil1`
-    FOREIGN KEY (`perfil_id`)
-    REFERENCES `growingdb`.`perfil` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
@@ -165,8 +115,9 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `growingdb`.`postagens` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `postagem` VARCHAR(500) NOT NULL,
+  `texto` VARCHAR(500) NOT NULL,
   `data_postagem` DATETIME NOT NULL,
+  `imagem` VARCHAR(200) NULL,
   `usuarios_id` INT NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_postagens_usuarios1_idx` (`usuarios_id` ASC) VISIBLE,
@@ -185,77 +136,28 @@ CREATE TABLE IF NOT EXISTS `growingdb`.`comentarios` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `data_cometario` DATETIME NOT NULL,
   `comentario` VARCHAR(500) NOT NULL,
+  `imagem` VARCHAR(200) NULL,
   `postagens_id` INT NOT NULL,
-  `usuarios_id` INT NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_comentarios_postagens1_idx` (`postagens_id` ASC) VISIBLE,
-  INDEX `fk_comentarios_usuarios1_idx` (`usuarios_id` ASC) VISIBLE,
   CONSTRAINT `fk_comentarios_postagens1`
     FOREIGN KEY (`postagens_id`)
     REFERENCES `growingdb`.`postagens` (`id`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_comentarios_usuarios1`
-    FOREIGN KEY (`usuarios_id`)
-    REFERENCES `growingdb`.`usuarios` (`id`)
-    ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `growingdb`.`servicos_produto`
+-- Table `growingdb`.`registro_portfolio`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `growingdb`.`servicos_produto` (
+CREATE TABLE IF NOT EXISTS `growingdb`.`registro_portfolio` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `servico` VARCHAR(150) NOT NULL,
-  `image1` VARCHAR(150) NULL,
-  `image2` VARCHAR(150) NULL,
-  `descricao` VARCHAR(500) NULL,
-  `valor` DECIMAL(5,2) NULL,
-  `tipo` INT NOT NULL,
-  `status` INT NOT NULL,
-  `perfil_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_servicos_perfil1_idx` (`perfil_id` ASC) VISIBLE,
-  CONSTRAINT `fk_servicos_perfil1`
-    FOREIGN KEY (`perfil_id`)
-    REFERENCES `growingdb`.`perfil` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `growingdb`.`portfoleo`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `growingdb`.`portfoleo` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `descricao` VARCHAR(500) NULL,
-  `image1` VARCHAR(150) NULL,
-  `image2` VARCHAR(150) NULL,
-  `image3` VARCHAR(150) NULL,
-  `image4` VARCHAR(150) NULL,
-  `status` INT NOT NULL,
-  `data_exec` DATETIME NOT NULL,
-  `perfil_id` INT NOT NULL,
-  `servicos_produto_id` INT NOT NULL,
+  `descricao` VARCHAR(300) NULL,
   `usuarios_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_portfoleo_perfil1_idx` (`perfil_id` ASC) VISIBLE,
-  INDEX `fk_portfoleo_servicos_produto1_idx` (`servicos_produto_id` ASC) VISIBLE,
-  INDEX `fk_portfoleo_usuarios1_idx` (`usuarios_id` ASC) VISIBLE,
-  CONSTRAINT `fk_portfoleo_perfil1`
-    FOREIGN KEY (`perfil_id`)
-    REFERENCES `growingdb`.`perfil` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_portfoleo_servicos_produto1`
-    FOREIGN KEY (`servicos_produto_id`)
-    REFERENCES `growingdb`.`servicos_produto` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_portfoleo_usuarios1`
+  INDEX `fk_registro_portfolio_usuarios1_idx` (`usuarios_id` ASC) VISIBLE,
+  CONSTRAINT `fk_registro_portfolio_usuarios1`
     FOREIGN KEY (`usuarios_id`)
     REFERENCES `growingdb`.`usuarios` (`id`)
     ON DELETE NO ACTION
@@ -264,66 +166,157 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `growingdb`.`agenda`
+-- Table `growingdb`.`servicos_especificos`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `growingdb`.`agenda` (
+CREATE TABLE IF NOT EXISTS `growingdb`.`servicos_especificos` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `servico` VARCHAR(50) NULL,
+  `preferencias_servicos_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_servicos_especificos_preferencias_servicos1_idx` (`preferencias_servicos_id` ASC) VISIBLE,
+  CONSTRAINT `fk_servicos_especificos_preferencias_servicos1`
+    FOREIGN KEY (`preferencias_servicos_id`)
+    REFERENCES `growingdb`.`preferencias_gerais` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `growingdb`.`usuarios__servicos_especificos`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `growingdb`.`usuarios__servicos_especificos` (
+  `usuarios_id` INT NOT NULL,
+  `servicos_especificos_id` INT NOT NULL,
+  `valor` DOUBLE(6,2) NOT NULL,
+  `imagem` VARCHAR(200) NOT NULL,
+  `descricao` VARCHAR(500) NOT NULL,
+  PRIMARY KEY (`usuarios_id`, `servicos_especificos_id`),
+  INDEX `fk_usuarios_has_servicos_especificos_servicos_especificos1_idx` (`servicos_especificos_id` ASC) VISIBLE,
+  INDEX `fk_usuarios_has_servicos_especificos_usuarios1_idx` (`usuarios_id` ASC) VISIBLE,
+  CONSTRAINT `fk_usuarios_has_servicos_especificos_usuarios1`
+    FOREIGN KEY (`usuarios_id`)
+    REFERENCES `growingdb`.`usuarios` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_usuarios_has_servicos_especificos_servicos_especificos1`
+    FOREIGN KEY (`servicos_especificos_id`)
+    REFERENCES `growingdb`.`servicos_especificos` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `growingdb`.`avaliacao_servico`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `growingdb`.`avaliacao_servico` (
+  `id` INT NOT NULL,
+  `nota` INT NULL,
+  `prestador_id` INT NOT NULL,
+  `servico_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_avaliacao_servico_usuarios__servicos_especificos1_idx` (`prestador_id` ASC, `servico_id` ASC) VISIBLE,
+  CONSTRAINT `fk_avaliacao_servico_usuarios__servicos_especificos1`
+    FOREIGN KEY (`prestador_id` , `servico_id`)
+    REFERENCES `growingdb`.`usuarios__servicos_especificos` (`usuarios_id` , `servicos_especificos_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `growingdb`.`compromissos`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `growingdb`.`compromissos` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `data_inicio` DATETIME NOT NULL,
-  `data_fim` DATETIME NOT NULL,
-  `recorrente` INT NOT NULL,
-  `tipo_recorrencia` VARCHAR(50) NULL,
-  `titulo` VARCHAR(150) NULL,
-  `descricao` VARCHAR(400) NULL,
-  `servicos_produto_id` INT NOT NULL,
-  `usuarios_id` INT NOT NULL,
+  `horario_fim` DATETIME NOT NULL,
+  `observacoes` VARCHAR(400) NULL,
+  `consumidor_id` INT NOT NULL,
+  `prestador_id` INT NOT NULL,
+  `servico_id` INT NOT NULL,
+  `avaliação_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_agenda_servicos_produto1_idx` (`servicos_produto_id` ASC) VISIBLE,
-  INDEX `fk_agenda_usuarios1_idx` (`usuarios_id` ASC) VISIBLE,
-  CONSTRAINT `fk_agenda_servicos_produto1`
-    FOREIGN KEY (`servicos_produto_id`)
-    REFERENCES `growingdb`.`servicos_produto` (`id`)
+  INDEX `fk_compromissos_usuarios1_idx` (`consumidor_id` ASC) VISIBLE,
+  INDEX `fk_compromissos_usuarios__servicos_especificos1_idx` (`prestador_id` ASC, `servico_id` ASC) VISIBLE,
+  INDEX `fk_compromissos_avaliação1_idx` (`avaliação_id` ASC) VISIBLE,
+  CONSTRAINT `fk_compromissos_usuarios1`
+    FOREIGN KEY (`consumidor_id`)
+    REFERENCES `growingdb`.`usuarios` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_agenda_usuarios1`
-    FOREIGN KEY (`usuarios_id`)
-    REFERENCES `growingdb`.`usuarios` (`id`)
+  CONSTRAINT `fk_compromissos_usuarios__servicos_especificos1`
+    FOREIGN KEY (`prestador_id` , `servico_id`)
+    REFERENCES `growingdb`.`usuarios__servicos_especificos` (`usuarios_id` , `servicos_especificos_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_compromissos_avaliação1`
+    FOREIGN KEY (`avaliação_id`)
+    REFERENCES `growingdb`.`avaliacao_servico` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `growingdb`.`chat`
+-- Table `growingdb`.`usuarios_preferencias`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `growingdb`.`chat` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `sessao` VARCHAR(245) NOT NULL,
-  `chat_sessao` VARCHAR(50) NULL,
-  `data_chat` DATETIME NOT NULL,
-  PRIMARY KEY (`id`))
+CREATE TABLE IF NOT EXISTS `growingdb`.`usuarios_preferencias` (
+  `usuarios_id` INT NOT NULL,
+  `preferencias_id` INT NOT NULL,
+  PRIMARY KEY (`usuarios_id`, `preferencias_id`),
+  INDEX `fk_usuarios_has_preferencias_preferencias1_idx` (`preferencias_id` ASC) VISIBLE,
+  INDEX `fk_usuarios_has_preferencias_usuarios1_idx` (`usuarios_id` ASC) VISIBLE,
+  CONSTRAINT `fk_usuarios_has_preferencias_usuarios1`
+    FOREIGN KEY (`usuarios_id`)
+    REFERENCES `growingdb`.`usuarios` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_usuarios_has_preferencias_preferencias1`
+    FOREIGN KEY (`preferencias_id`)
+    REFERENCES `growingdb`.`preferencias_gerais` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `growingdb`.`mensagens_chat`
+-- Table `growingdb`.`imagens_portfolio`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `growingdb`.`mensagens_chat` (
+CREATE TABLE IF NOT EXISTS `growingdb`.`imagens_portfolio` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `mensagens` VARCHAR(450) NOT NULL,
-  `data_mensagem` DATETIME NOT NULL,
-  `chat_id` INT NOT NULL,
-  `usuarios_id` INT NOT NULL,
+  `imagem` VARCHAR(200) NOT NULL,
+  `portfolio_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_mensagens_chat_chat1_idx` (`chat_id` ASC) VISIBLE,
-  INDEX `fk_mensagens_chat_usuarios1_idx` (`usuarios_id` ASC) VISIBLE,
-  CONSTRAINT `fk_mensagens_chat_chat1`
-    FOREIGN KEY (`chat_id`)
-    REFERENCES `growingdb`.`chat` (`id`)
+  INDEX `fk_imagens_portfolio_portfolio1_idx` (`portfolio_id` ASC) VISIBLE,
+  CONSTRAINT `fk_imagens_portfolio_portfolio1`
+    FOREIGN KEY (`portfolio_id`)
+    REFERENCES `growingdb`.`registro_portfolio` (`id`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_mensagens_chat_usuarios1`
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `growingdb`.`avaliacao_usuario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `growingdb`.`avaliacao_usuario` (
+  `id` INT NOT NULL,
+  `nota` INT NULL,
+  `usuarios_id` INT NOT NULL,
+  `compromissos_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_avaliacao_usuario_usuarios1_idx` (`usuarios_id` ASC) VISIBLE,
+  INDEX `fk_avaliacao_usuario_compromissos1_idx` (`compromissos_id` ASC) VISIBLE,
+  CONSTRAINT `fk_avaliacao_usuario_usuarios1`
     FOREIGN KEY (`usuarios_id`)
     REFERENCES `growingdb`.`usuarios` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_avaliacao_usuario_compromissos1`
+    FOREIGN KEY (`compromissos_id`)
+    REFERENCES `growingdb`.`compromissos` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
